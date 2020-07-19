ca6481750c46cbbecb514eca70563fb3f32088a2
John <indianajohn@github>
2020-07-19 16:04:23 -0700

    Passing push_back tests.

--- stl_containers/src/vector.hpp
+++ stl_containers/src/vector.hpp
@@ -4,15 +4,34 @@
 namespace prac {
 template <typename T> class vector {
 public:
-  vector(const size_t &num_elem = 0, const T &default_value = T()) {
-    // TODO
+  vector(size_t num_elements = 0, T default_value = T())
+      : m_num_elements(num_elements) {
+    m_num_allocated = 10 + num_elements;
+    m_storage = new T[m_num_allocated];
+    for (size_t i = 0; i < m_num_elements; i++) {
+      m_storage[i] = default_value;
+    }
   }
+
   void push_back(const T &new_elem) {
-    // TODO
+    if (m_num_elements >= m_num_allocated) {
+      T *new_storage = new T[m_num_allocated * 2];
+      for (size_t i = 0; i < m_num_elements; i++) {
+        new_storage[i] = m_storage[i];
+      }
+      delete[] m_storage;
+      m_num_allocated = 2 * m_num_allocated;
+      m_storage = new_storage;
+    }
+    m_storage[m_num_elements] = new_elem;
+    m_num_elements++;
   }
-  size_t size() const {
-    // TODO
-    return 0;
-  }
+
+  size_t size() const { return m_num_elements; }
+
+private:
+  T *m_storage;
+  size_t m_num_allocated;
+  size_t m_num_elements;
 };
 }; // namespace prac
